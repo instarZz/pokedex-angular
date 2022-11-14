1d83eac7ccc27bfd14ed89ea1b75109a8a19e0ba
instarZz <instarz.je@gmail.com>
2022-11-14 15:48:10 +0100

    component html + affichage de la liste complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { POKEMONS } from './mock-pokemon-list';
 import { Pokemon } from './pokemon';
 
-
 @Component({
   selector: 'app-root',
-  template: `<h1>Liste de Pokemons</h1>`
+  templateUrl: 'app.component.html',
 })
+
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
 
