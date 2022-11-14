05cca50519636e727ecea9119cbe59fff49464de
instarZz <instarz.je@gmail.com>
2022-11-14 16:28:03 +0100

    ngIf + +element pour le rendre number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,29 @@ import { Pokemon } from './pokemon';
 })
 
 export class AppComponent implements OnInit {
+
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
 
   ngOnInit(): void {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
   } 
 
-  selectPokemon(pokemon: Pokemon){
-    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  selectPokemon(pokemonId: string){
+
+    const id = +pokemonId;
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+
+    if(pokemon){
+
+      console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+
+    } else {
+
+      console.log(`Vous avez demandé un pokémon qui n'existe pas`);
+      this.pokemonSelected = pokemon;
+
+    }
   }
 }
