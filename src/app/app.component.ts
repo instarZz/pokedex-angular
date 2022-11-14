13ee3cf471d9c11d0ae1e1dc97bf010727c9b241
instarZz <instarz.je@gmail.com>
2022-11-14 15:06:31 +0100

    setup ng serve OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{title}}!</h1>`
+  template: `<h1>Welcome to {{ pokemonList[0] }}!</h1>`
 })
 export class AppComponent {
-  title = 'ng-pokemon-app';
+  pokemonList = ['Bulbizarre', 'Salameche', 'Carapuce'];
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+    this.selectPokemon('Salameche');
+  } 
+
+  selectPokemon(pokemonName: string){
+    console.log(`Vous avez cliqué sur le pokémon ${pokemonName}`);
+  }
 }
