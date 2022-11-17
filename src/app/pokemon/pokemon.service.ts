c90cbdb12c7de870818665cf844355d1f652c8b9
instarZz <instarz.je@gmail.com>
2022-11-17 15:48:14 +0100

    clean tree for update Angular15

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -15,14 +15,14 @@ export class PokemonService {
     return this.http.get<Pokemon[]>('api/pokemon').pipe(
       tap((response) => this.log(response)), // tap = un log pour un Observable
       catchError((error) => this.handleError(error, []))
-    )
+    );
   }
 
   getPokemonById(pokemonId: number): Observable<Pokemon|undefined> {
     return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
       tap((response) => this.log(response)), // tap = un log pour un Observable
       catchError((error) => this.handleError(error, undefined))
-    )
+    );
   }
 
   private handleError(error: Error, errorValue: any){
